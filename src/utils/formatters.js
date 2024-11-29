b2dac122cbd762b825b9343d657cb41574bead3c
Frank Wild <bondyfan@me.com>
2024-11-29 18:29:44 +0100

    0 views problem fix

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,4 +1,6 @@
 export const formatViews = (views) => {
+  // If views is undefined (not loaded yet), return empty string instead of '0'
+  if (views === undefined) return '';
   if (!views) return '0';
   if (views >= 1000000) {
     return (Math.floor((views / 100000)) / 10).toFixed(1) + 'M';
